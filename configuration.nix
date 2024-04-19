849b31a66cc56710f720855008b3753bf67938c1
SeekerOfAsh <michael.muellerdelossantos@outlook.com>
2024-04-19 13:22:39 +0200

    enabled flakes

--- configuration.nix
+++ configuration.nix
@@ -41,4 +41,7 @@
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "23.11"; # Did you read the comment?
 
+  # Enable flakes
+   nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
 }
