47b6ab4b2aab01c36a3f651d236838f2a48673d8
SeekerOfAsh <michael.muellerdelossantos@outlook.com>
2024-04-21 20:29:17 +0200

    forced anime wallpaper and added polkit service

--- software/desktopEnvironment.nix
+++ software/desktopEnvironment.nix
@@ -41,6 +41,29 @@
     swww
   ];
 
+  # enable polkit
+  # todo: move polkit to different file
+  security.polkit.enable = true;
+
+  systemd = 
+  {
+    user.services.polkit-gnome-authentication-agent-1 = 
+    {
+      description = "polkit-gnome-authentication-agent-1";
+      wantedBy = [ "graphical-session.target" ];
+      wants = [ "graphical-session.target" ];
+      after = [ "graphical-session.target" ];
+      serviceConfig = 
+      { 
+        Type = "simple";
+        ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+        Restart = "on-failure";
+        RestartSec = 1;
+        TimeoutStopSec = 10;
+      };
+    };
+  };
+
   # custom fonts
   fonts.fontDir.enable = true;
   fonts.fonts = with pkgs;
