9a3c56465b4706e437b836bc9b6ad942666fc8a5
SeekerOfAsh <michael.muellerdelossantos@outlook.com>
2024-04-21 12:21:45 +0200

    changed to hyprland

--- software/desktopEnvironment.nix
+++ software/desktopEnvironment.nix
@@ -1,12 +1,16 @@
+{config, pkgs, ... }: 
 {
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
   # Enable GNOME Desktop Environment.
-  # services.xserver.displayManager.gdm.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
   # services.xserver.desktopManager.gnome.enable = true;
 
   # Enable KDE Desktop Environment.
-  services.xserver.displayManager.sddm.enable = true;
-  services.desktopManager.plasma6.enable = true;
+  # services.xserver.displayManager.sddm.enable = true;
+  # services.desktopManager.plasma6.enable = true;
+
+  # Enable Hyprland Window Manager
+  programs.hyprland.enable = true;
 }
