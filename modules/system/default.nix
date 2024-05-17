35e209d449e2ff391cb7e0756aa7bbd9584dc880
SeekerOfAsh <michael.muellerdelossantos@outlook.com>
2024-05-18 01:54:05 +0200

    performed refactoring

--- modules/system/default.nix
+++ modules/system/default.nix
@@ -2,11 +2,13 @@
 {
   imports = 
   [
+    ./flatpak.nix
     ./fonts.nix
     ./kernel.nix
     ./packages.nix
     ./security.nix
     ./services.nix
+    ./sessionVariables.nix
     ./steam.nix
   ];
 }
