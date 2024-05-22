26b27f3c94715b0fb42f5019a4dacc48ab7bff8b
SeekerOfAsh <michael.muellerdelossantos@outlook.com>
2024-05-22 16:08:27 +0200

    added missing environment variables

--- modules/system/sessionVariables.nix
+++ modules/system/sessionVariables.nix
@@ -9,5 +9,13 @@ in
   {
     NIXOS_OZONE_WL = "1";
     XCURSOR_SIZE = cursorSize;
+    XDG_SESSION_DESKTOP = "Hyprland";
+    GDK_BACKEND = "wayland,x11";
+    CLUTTER_BACKEND = "wayland";
+    QT_QPA_PLATFORM = "wayland;xcb";
+    QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
+    QT_AUTO_SCREEN_SCALE_FACTOR = "1";
+    SDL_VIDEODRIVER = "wayland";
+    #WLR_DRM_NO_ATOMIC = "1";
   };
 }
