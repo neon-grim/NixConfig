e9df1e91a94dcbd6bb68f12003ff2ee844f4eee3
SeekerOfAsh <michael.muellerdelossantos@outlook.com>
2024-05-28 16:48:56 +0200

    added missing environment variables

--- modules/system/sessionVariables.nix
+++ modules/system/sessionVariables.nix
@@ -7,15 +7,29 @@ in
 {
   environment.sessionVariables = 
   {
-    NIXOS_OZONE_WL = "1";
-    XCURSOR_SIZE = cursorSize;
-    XDG_SESSION_DESKTOP = "Hyprland";
-    GDK_BACKEND = "wayland,x11";
     CLUTTER_BACKEND = "wayland";
+    GDK_BACKEND = "wayland,x11";
+    NIXOS_OZONE_WL = "1";
     QT_QPA_PLATFORM = "wayland;xcb";
     QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
     QT_AUTO_SCREEN_SCALE_FACTOR = "1";
     SDL_VIDEODRIVER = "wayland";
-    #WLR_DRM_NO_ATOMIC = "1";
+    WLR_DRM_NO_ATOMIC = "1";
+    XDG_SESSION_DESKTOP = "Hyprland";
+    XCURSOR_SIZE = cursorSize;
+    # User dirs
+    XDG_DESKTOP_DIR= "$HOME/Desktop";
+    XDG_DOCUMENTS_DIR= "$HOME/Documents";
+    XDG_DOWNLOAD_DIR= "$HOME/Downloads";
+    XDG_MUSIC_DIR= "$HOME/Music";
+    XDG_PICTURES_DIR= "$HOME/Pictures";
+    XDG_PUBLICSHARE_DIR= "$HOME/Public";
+    XDG_TEMPLATES_DIR= "$HOME/Templates";
+    XDG_VIDEOS_DIR= "$HOME/Videos";
+    # Config dirs
+    XDG_CACHE_HOME = "$HOME/.cache";
+    XDG_CONFIG_HOME = "$HOME/.config";
+    XDG_DATA_HOME = "$HOME/.local/share";
+    XDG_STATE_HOME = "$HOME/.local/state";
   };
 }
