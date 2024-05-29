808136c102dce7ac9c9918d06470e0fef837ee17
SeekerOfAsh <michael.muellerdelossantos@outlook.com>
2024-05-29 20:42:05 +0200

    fixed storage issue and updated system

--- hosts/SmelterDeamon/storage-config.nix
+++ hosts/SmelterDeamon/storage-config.nix
@@ -4,18 +4,18 @@
   {
     device = "/dev/disk/by-label/NVME4SSD2";
     fsType = "ext4";
-    options = [ "nofail" ]; 
+    options = ["nofail" "x-gvfs-show"]; 
   };
   fileSystems."/mnt/NVME4SSD1" = 
   {
     device = "/dev/disk/by-label/NVME4SSD1";
     fsType = "ext4";
-    options = [ "nofail" ];
+    options = ["nofail" "x-gvfs-show"];
   };
   fileSystems."/mnt/SATASSD1" = 
   {
     device = "/dev/disk/by-label/SATASSD1";
     fsType = "ext4";
-    options = [ "nofail" ];
+    options = ["nofail" "x-gvfs-show"];
   };
 }
