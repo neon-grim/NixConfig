4ab76d229852d88c3ba63c0bd18925d4e42abd54
SeekerOfAsh <michael.muellerdelossantos@outlook.com>
2024-06-23 00:01:45 +0200

    disabled env variable

--- modules/system/system/environmentVariables.nix
+++ modules/system/system/environmentVariables.nix
@@ -12,7 +12,7 @@ in
     QT_QPA_PLATFORM = "wayland;xcb";
     QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
     QT_AUTO_SCREEN_SCALE_FACTOR = "1";
-    SDL_VIDEODRIVER = "wayland";
+    #SDL_VIDEODRIVER = "wayland";
     XDG_SESSION_DESKTOP = "Hyprland";
     XCURSOR_SIZE = cursorSize;
     # User dirs
