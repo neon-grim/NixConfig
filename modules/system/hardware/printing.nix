10b33c12508c4111108995e53bfe3f55a58b812d
SeekerOfAsh <michael.muellerdelossantos@outlook.com>
2024-08-13 20:41:27 +0200

    added hp printing drivers

--- modules/system/hardware/printing.nix
+++ modules/system/hardware/printing.nix
@@ -1,10 +1,14 @@
-{...}:
+{pkgs, ...}:
 {
   programs.system-config-printer.enable = true;
   services =
   {
     system-config-printer.enable = true;
-    printing.enable = true;
+    printing =
+    {
+      enable = true;
+      drivers = with pkgs; [ hplip ];
+    };
     avahi = 
     {
       enable = true;
