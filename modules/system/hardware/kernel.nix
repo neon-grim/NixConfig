f7e4143deebce711fbdabbffcccc00b15303c686
SeekerOfAsh <michael.muellerdelossantos@outlook.com>
2024-09-29 08:51:06 +0200

    change tweak script to optonally set specific settings, changed to mainline kernel, updated system

--- modules/system/hardware/kernel.nix
+++ modules/system/hardware/kernel.nix
@@ -9,7 +9,7 @@
       systemd-boot.enable = true;
       efi.canTouchEfiVariables = true;
     };
-    kernelPackages = pkgs.linuxPackages_xanmod_latest;
+    kernelPackages = pkgs.linuxPackages_latest;
     kernelParams = 
     [ 
       "quiet" 
--- scripts/hyprlandTweaksOn.sh
+++ scripts/hyprlandTweaksOn.sh
@@ -1,11 +1,28 @@
 #!/usr/bin/env bash
 
-refreshRate=240
+adaptiveSync="0"
+bitdepth=""
+cursor="false";
+directScanout="true"
+maxRefreshRate="120.00"
+variableFrameRate="true"
 
-while getopts ":r" opt; do
+while getopts ":abdmv" opt; do
   case $opt in
-    r)
-      refreshRate=120
+    a)
+      adaptiveSync="2"
+      ;;
+    b)
+      bitdepth="bitdepth, 10"
+      ;;
+    d)
+      directScanout="true"
+      ;;
+    m)
+      maxRefreshRate="240.00"
+      ;;
+    v)
+      variableFrameRate="false"
       ;;
     ?)
       echo 'Unknown paramter'
@@ -14,15 +31,21 @@ while getopts ":r" opt; do
   esac
 done
 
+if [ $adaptiveSync == "2" ]
+then
+  cursor="true"
+fi
+
 hyprctl --batch "\
   keyword animations:enabled false; \
   keyword decoration:blur:enabled false; \
   keyword decoration:drop_shadow false; \
   keyword cursor:min_refresh_rate 0; \
-  keyword cursor:no_hardware_cursors true; \
-  keyword cursor:no_break_fs_vrr true; \
+  keyword cursor:no_hardware_cursors ${cursor}; \
+  keyword cursor:no_break_fs_vrr ${cursor}; \
   keyword general:allow_tearing true; \
-  keyword render:direct_scanout true; \
-  keyword monitor desc:Samsung Electric Company Odyssey G95SC H1AK500000, 5120x1440@${refreshRate}, 1440x640, 1, vrr, 2, bitdepth, 10;"
+  keyword misc:vfr ${variableFrameRate}; \
+  keyword render:direct_scanout ${directScanout}; \
+  keyword monitor desc:Samsung Electric Company Odyssey G95SC H1AK500000, 5120x1440@${maxRefreshRate}, 1440x640, 1, vrr, ${adaptiveSync}, ${bitdepth};"
 
-sleep 20s
+sleep 10s
