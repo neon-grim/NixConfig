bdb47031d635a9123fdb3d2f3bf9b6ea405bbf95
SeekerOfAsh <michael.muellerdelossantos@outlook.com>
2024-10-09 19:25:52 +0200

    added waybar module config back into waybar module

--- modules/homeManager/compositorTools/waybar.nix
+++ modules/homeManager/compositorTools/waybar.nix
@@ -1,37 +1,120 @@
-{...}:
+{pkgs, lib, ...}:
+let
+  calendar = "${lib.getExe pkgs.thunderbird}";
+  performanceApp = "${lib.getExe' pkgs.corectrl "corectrl"}";
+  taskManager = "${lib.getExe pkgs.resources}";
+  volumeControl = "${lib.getExe pkgs.pavucontrol}";
+  powerMenu = "pkill wofi; sleep 0.1 && wofiPowerMenu";
+in
 {
   imports =
   [
     ./waybar/waybarStyle.nix
-    ./waybar/waybarModules.nix
   ];
   programs.waybar =
   {
     enable = true;
     settings =
-    [
+    [{
+      layer = "top";
+      position = "top";
+      modules-left =
+      [
+        "hyprland/workspaces"
+        "sway/workspaces"
+      ];
+      modules-center =
+      [ 
+        "clock"
+      ];
+      modules-right =
+      [
+        "pulseaudio"
+        "memory"
+        "cpu"
+        "custom/notification"
+        "tray"
+        "custom/powermenu"
+      ];
+       "tray" =
       {
-        layer = "top";
-        position = "top";
-        modules-left =
-        [
-          "hyprland/workspaces"
-          "sway/workspaces"
-        ];
-        modules-center =
-        [ 
-          "clock"
-        ];
-        modules-right =
-        [
-          "pulseaudio"
-          "memory"
-          "cpu"
-          "custom/notification"
-          "tray"
-          "custom/powermenu"
-        ];
-      }
-    ];
+        spacing = 12;
+        icon-size = 18;
+        reverse-direction = true;
+      };
+      "pulseaudio" =
+      {
+        format = "{icon} {volume}%";
+        format-muted = "󰖁 {volume}%";
+        format-icons = 
+        {
+          default = ["" ""];
+        };
+        on-click = "sleep 0.1 && ${volumeControl}";
+      };
+      "clock" =
+      {
+        interval = 1;
+        format = "{:%R  %A %b %d}";
+        on-click = "sleep 0.1 && ${calendar}";
+      };
+      "memory" =
+      {
+        interval = 1;
+        format = " {percentage}%";
+        states = {
+          "warning" = 85;
+        };
+        on-click = "sleep 0.1 && ${taskManager}";
+      };
+      "cpu" =
+      {
+        interval = 1;
+        format = " {usage}%";
+        on-click = "sleep 0.1 && ${performanceApp}";
+      };
+      "custom/powermenu" =
+      {
+        format = "";
+        on-click = "sleep 0.1 && ${powerMenu}";
+        tooltip = false;
+      };
+      "custom/notification" =
+      {
+        tooltip = false;
+        format = "{icon} {}";
+        format-icons = {
+          notification = "";
+          none = "";
+          dnd-notification = "";
+          dnd-none = "";
+          inhibited-notification = "";
+          inhibited-none = "";
+          dnd-inhibited-notification = "";
+          dnd-inhibited-none = "";
+        };
+        return-type = "json";
+        exec-if = "which swaync-client";
+        exec = "swaync-client -swb";
+        on-click = "sleep 0.1 && swaync-client -t -sw";
+        on-click-right = "swaync-client -d -sw";
+        escape = true;
+      };
+      "hyprland/workspaces" =
+      {
+        on-scroll-up = "hyprctl dispatch workspace e+1";
+        on-scroll-down = "hyprctl dispatch workspace e-1";
+        on-click = "activate";
+        format = "{icon}";
+        format-icons = 
+        {
+          default = "";
+          active = "";
+          empty = "";
+          urgent = "";
+          "1" = "";
+        };
+      };
+    }];
   };
 }
