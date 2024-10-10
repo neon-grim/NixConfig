8d1ea07f7e7a29730d5ef6a80e8a1113322d79eb
SeekerOfAsh <michael.muellerdelossantos@outlook.com>
2024-10-10 16:11:18 +0200

    organized options in amdvlk alphabetically

--- modules/nixos/hardware/amdvlk.nix
+++ modules/nixos/hardware/amdvlk.nix
@@ -2,13 +2,13 @@
 {
   config = lib.mkIf (config.desktop.drivers.amd.enable && config.desktop.drivers.amd.amdvlk)
   {
-    hardware.amdgpu.amdvlk =
-    {
-      enable = true;
-    };
     environment.variables =
     {
       AMD_VULKAN_ICD = "RADV";
+    };
+    hardware.amdgpu.amdvlk =
+    {
+      enable = true;
     };
   };
 }
