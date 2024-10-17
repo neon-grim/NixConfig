25ea9f9f25f769811f21a8c9e231c2bb70e893bb
SeekerOfAsh <michael.muellerdelossantos@outlook.com>
2024-10-17 22:14:23 +0200

    added OBS bindings, removed modules which werent use

--- modules/homeManager/compositors/hyprland/hyprlandBindings.nix
+++ modules/homeManager/compositors/hyprland/hyprlandBindings.nix
@@ -1,6 +1,6 @@
 {pkgs, lib, ...}:
 let
-    mainMon = "SUPER";
+    mainMod = "SUPER";
 in
 {
   wayland.windowManager.hyprland.settings =
@@ -11,8 +11,8 @@ in
     };
     bindm =
     [
-      "${mainMon}, mouse:272, movewindow"
-      "${mainMon}, mouse:273, resizewindow"
+      "${mainMod}, mouse:272, movewindow"
+      "${mainMod}, mouse:273, resizewindow"
     ];
     bindl =
     [
@@ -27,28 +27,32 @@ in
     ];
     bind =
     let 
-      mainModShift = "${mainMon} SHIFT";
-      mainModAlt = "${mainMon} ALT_L";
-      mainModControl = "${mainMon} CONTROL_L";
+      mainModShift = "${mainMod} SHIFT";
+      mainModAlt = "${mainMod} ALT_L";
+      mainModControl = "${mainMod} CONTROL_L";
       menu = "pkill wofi; sleep 0.1 && wofi -S drun --width 500 --height 600";
       powerMenu = "pkill wofi; sleep 0.1 && wofiPowerMenu";
     in
     [
       # Execute default programs and actions
-      "${mainMon}, Return, exec, ${lib.getExe' pkgs.terminator "terminator"}"
-      "${mainMon}, E, exec, ${lib.getExe pkgs.pcmanfm}"
-      "${mainMon}, B, exec, ${lib.getExe pkgs.librewolf}"
-      "${mainMon}, R, exec, ${menu}"
-      "${mainMon}, L, exec, ${powerMenu}"
-      "${mainMon}, K, killactive,"
-      "${mainMon}, F, fullscreen,"
+      "${mainMod}, Return, exec, ${lib.getExe' pkgs.terminator "terminator"}"
+      "${mainMod}, E, exec, ${lib.getExe pkgs.pcmanfm}"
+      "${mainMod}, B, exec, ${lib.getExe pkgs.librewolf}"
+      "${mainMod}, R, exec, ${menu}"
+      "${mainMod}, O, exec, ${lib.getExe pkgs.obs-studio}"
+      "${mainMod}, L, exec, ${powerMenu}"
+      "${mainMod}, K, killactive,"
+      "${mainMod}, F, fullscreen,"
       "${mainModShift}, L, exit,"
       "${mainModShift}, V, togglefloating,"
+      # OBS shortcuts
+      "${mainMod}, F1, pass, ^(com\.obsproject\.Studio)$"
+      "${mainMod}, F2, pass, ^(com\.obsproject\.Studio)$"
       # Change focused window
-      "${mainMon}, left, movefocus, l"
-      "${mainMon}, right, movefocus, r"
-      "${mainMon}, up, movefocus, u"
-      "${mainMon}, down, movefocus, d"
+      "${mainMod}, left, movefocus, l"
+      "${mainMod}, right, movefocus, r"
+      "${mainMod}, up, movefocus, u"
+      "${mainMod}, down, movefocus, d"
       # Move focused window within workspace
       "${mainModShift}, left, movewindow, l"
       "${mainModShift}, right, movewindow, r"
@@ -58,10 +62,10 @@ in
       "${mainModControl}, left, layoutmsg, rollnext"
       "${mainModControl}, right, layoutmsg, rollprev"
       # Make focused window Master
-      "${mainMon}, home, layoutmsg, swapwithmaster"
+      "${mainMod}, home, layoutmsg, swapwithmaster"
       # Navigate between workspaces on the same monitor
-      "${mainMon}, PAGE_DOWN, exec, hyprnome -k"
-      "${mainMon}, PAGE_UP, exec, hyprnome -p -k -n"
+      "${mainMod}, PAGE_DOWN, exec, hyprnome -k"
+      "${mainMod}, PAGE_UP, exec, hyprnome -p -k -n"
       # Move focused window between workspaces on the same monitor
       "${mainModShift}, PAGE_DOWN, exec, hyprnome -m"
       "${mainModShift}, PAGE_UP, exec, hyprnome -p -m -n"
@@ -69,16 +73,16 @@ in
       "${mainModControl}, PAGE_UP, movewindow, mon:-1"
       "${mainModControl}, PAGE_DOWN, movewindow, mon:+1"
       # Example special workspace (scratchpad)
-      "${mainMon}, S, togglespecialworkspace, magic"
+      "${mainMod}, S, togglespecialworkspace, magic"
       "${mainModShift}, S, movetoworkspace, special:magic"
       "${mainModControl}, S, movetoworkspace, e+0"
       # Scroll through existing workspaces
       "${mainModAlt}, PAGE_UP, workspace, e+1"
       "${mainModAlt}, PAGE_DOWN, workspace, e-1"
       # Screenshot
-      "${mainMon}, F9, exec, hyprshot -m region --freeze"
-      "${mainMon}, F10, exec, hyprshot -m window -m active --freeze"
-      "${mainMon}, F11, exec, hyprshot -m output -m active --freeze"
+      "${mainMod}, F9, exec, hyprshot -m region --freeze"
+      "${mainMod}, F10, exec, hyprshot -m window -m active --freeze"
+      "${mainMod}, F11, exec, hyprshot -m output -m active --freeze"
       # Save clients to file
       "${mainModShift}, F5, exec, hyprctl clients | tee ~/Documents/client.txt"
     ];
