43dd3ce91aa4f126b82b05d72bc39f98f2fcf0a4
SeekerOfAsh <michael.muellerdelossantos@outlook.com>
2024-10-29 19:14:40 +0100

    Changed mon config

--- hosts/SmelterDeamon/hostSpecific/homeManager/monitorConfig.nix
+++ hosts/SmelterDeamon/hostSpecific/homeManager/monitorConfig.nix
@@ -1,7 +1,7 @@
 {config, ...}:
 let
   mainMonitorRes = "5120x1440";
-  mainMonitorPos = "1440x640";
+  mainMonitorPos = "0x640";
   mainMonitor = "desc:Samsung Electric Company Odyssey G95SC H1AK500000";
   bottomMonitor = "desc:Invalid Vendor Codename - RTK Verbatim MT14 demoset-1";
   sideMonitor = "desc:Acer Technologies ED323QUR";
@@ -21,8 +21,8 @@ in
   wayland.windowManager.hyprland.settings.monitor =
   [
     "${mainMonitor}, ${mainMonitorRes}@120.00, ${mainMonitorPos}, 1"
-    "${bottomMonitor}, preferred, 2720x2080, 1"
-    "${sideMonitor}, preferred, 0x0, 1, transform, 3"
+    "${bottomMonitor}, preferred, 1600x2080, 1"
+    "${sideMonitor}, preferred, 5120x0, 1, transform, 1"
     ", preferred, auto, 1"
   ];
   services.hyprpaper.settings =
