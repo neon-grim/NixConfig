7c72940af7c6507be0f10fdc6ce4349037930b7a
NeonGrim <michael.muellerdelossantos@outlook.com>
2024-11-03 20:07:46 +0100

    added notifications to hyprTweaks

--- modules/homeManager/compositorTools/hyprTweaks.nix
+++ modules/homeManager/compositorTools/hyprTweaks.nix
@@ -1,7 +1,7 @@
 {pkgs, lib, inputs, config, ...}:
 let
+  dunstify = lib.getExe' pkgs.dunst "dunstify";
   hyprctl = lib.getExe' inputs.hyprland.packages.x86_64-linux.hyprland "hyprctl";
-  notifySend = lib.getExe pkgs.libnotify;
   mainMonName = config.desktop.system.mainMon.name;
   mainMonRes = config.desktop.system.mainMon.res;
   mainMonOc = config.desktop.system.mainMon.oc;
@@ -23,8 +23,8 @@ in
       notifyMessage="Tweaks on:"
       
       if [[ "${mainMonName}" == "" || "${mainMonRes}" == "" || "${mainMonOc}" == "" || "${mainMonPos}" == "" ]]; then
-        ${notifySend} -u critical -a 'Hyprland Tweaks' 'Missing main monitor config!'
-        exit 2
+        ${dunstify} -u critical "$notifyGroup" "Missing main monitor config!"
+        exit 1
       fi
       
       monitorConfig="monitor ${mainMonName}, ${mainMonRes}@${mainMonOc}, ${mainMonPos}, 1"
@@ -49,16 +49,16 @@ in
             ;;
           p)
             monitorConfig="monitor ${mainMonName}, preferred, ${mainMonPos}, 1"
-            notifyMessage+=" 120hz"
+            notifyMessage+=" preferred"
             ;;
           r)
             ${hyprctl} reload
-            ${notifySend} -u normal -a "$notifyGroup" "Reloaded Hyprland."
+            ${dunstify} -u normal "$notifyGroup" "Reloaded Hyprland."
             exit 0
             ;;
           ?)
-            ${notifySend} -u critical -a "$notifyGroup" "Unknown parameters!"
-            exit 1
+            ${dunstify} -u critical "$notifyGroup" "Unknown parameters!"
+            exit 2
             ;;
         esac
       done
@@ -77,7 +77,7 @@ in
       
       ${hyprctl} --batch $hyprCommand
       
-      ${notifySend} -u normal -a "$notifyGroup" "$notifyMessage"
+      ${dunstify} -u normal "$notifyGroup" "$notifyMessage"
       
       sleep 20s
     ''
