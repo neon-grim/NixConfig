27cfcb443418b4328949983a59d8a79b5b65bfcd
NeonGrim <105853956+neon-grim@users.noreply.github.com>
2025-01-07 17:00:59 +0100

    remove zen browser (#27)

--- flake.nix
+++ flake.nix
@@ -3,7 +3,6 @@
   inputs =
   {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    zen-browser.url = "github:MarceColl/zen-browser-flake";
     home-manager =
     {
       inputs.nixpkgs.follows = "nixpkgs";
