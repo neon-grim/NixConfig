20e56ed0ee454da679d01e3079ca1d42d58c0afb
NeonGrim <michael.muellerdelossantos@outlook.com>
2025-05-31 00:35:48 +0200

    moved lact config to new module

---
+++ modules/nixos/hardware/graphicsConfig/lact.nix
@@ -0,0 +1,27 @@
+{pkgs, ...}:
+{
+  environment.systemPackages = with pkgs;
+  [
+    lact
+  ];
+  nixpkgs.overlays = 
+  [
+    (final: prev: {
+      lact = final.callPackage "${inputs.lact}/pkgs/by-name/la/lact/package.nix"
+      {
+        hwdata = final.callPackage "${inputs.lact}/pkgs/by-name/hw/hwdata/package.nix" { };
+      };
+    })
+  ];
+  systemd.services.lactd =
+  {
+    enable = true;
+    after = [ "multi-user.target" ];
+    description = "AMDGPU Control Daemon";
+    serviceConfig =
+    {
+      ExecStart = "${pkgs.lact}/bin/lact daemon";
+    };
+    wantedBy = [ "multi-user.target" ];
+  };
+}
