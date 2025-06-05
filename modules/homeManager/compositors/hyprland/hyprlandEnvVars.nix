23727604a8f44d8f7f813871ca31fce3bf8413ab
NeonGrim <michael.muellerdelossantos@outlook.com>
2025-06-05 20:58:52 +0200

    Added new env vars for hyprland session

--- modules/homeManager/compositors/hyprland/hyprlandEnvVars.nix
+++ modules/homeManager/compositors/hyprland/hyprlandEnvVars.nix
@@ -7,13 +7,18 @@ in
   {
     env =
     [
+      "XDG_SESSION_TYPE,wayland"
+      "XDG_CURRENT_DESKTOP,Hyprland"
+      "XDG_SESSION_DESKTOP,Hyprland"
       "CLUTTER_BACKEND,wayland"
-      "GDK_BACKEND,wayland,x11,*"
+      "GDK_BACKEND,wayland"
+      "SDL_VIDEODRIVER,wayland"
       "QT_QPA_PLATFORM,wayland;xcb"
-      "QT_AUTO_SCREEN_SCALE_FACTOR,1"
+      "QT_AUTO_SCREEN_SCALE_FACTOR,1.25"
       "QT_WAYLAND_DISABLE_WINDOWDECORATION,1"
       "XCURSOR_SIZE,${cursorSize}"
       "HYPRSHOT_DIR,$XDG_PICTURES_DIR/ScreenShots"
+      "STEAM_FORCE_DESKTOPUI_SCALING,1.25"
     ];
   };
 }
