69a96566daf9b9c16f8347c34afcac9dd3b85a74
NeonGrim <michael.muellerdelossantos@outlook.com>
2025-06-12 10:52:09 +0200

    removed unused conditional statement, mainline kernel is the only package used

--- modules/nixos/hardware/kernel.nix
+++ modules/nixos/hardware/kernel.nix
@@ -11,12 +11,7 @@ in
   ];
   boot =
   {
-    kernelPackages = 
-    (
-      if (config.desktop.kernel.name == "lqx") then pkgs.linuxPackages_lqx
-      else if (config.desktop.kernel.name == "zen") then pkgs.linuxPackages_zen
-      else pkgs.linuxPackages_latest
-    );
+    kernelPackages = pkgs.linuxPackages_latest;
     kernelParams =
     [
       "quiet"
