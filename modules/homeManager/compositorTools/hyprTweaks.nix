ce577e1a578f53e681bbbfd6008e00b2669b57cd
NeonGrim <michael.muellerdelossantos@outlook.com>
2025-10-11 19:19:28 +0200

    reworked and cleaned up hyprTweaks

--- modules/homeManager/compositorTools/hyprTweaks.nix
+++ modules/homeManager/compositorTools/hyprTweaks.nix
@@ -1,56 +1,109 @@
 {pkgs, lib, config, ...}:
 let
+  # App Executables
   dunstify = lib.getExe' pkgs.dunst "dunstify";
   hyprctl = lib.getExe' pkgs.hyprland "hyprctl";
   xrandr = lib.getExe pkgs.xorg.xrandr;
+  # Main Monitor Config
   mainMonName = config.desktop.system.mainMon.name;
+  mainMonDesc = config.desktop.system.mainMon.desc;
   mainMonRes = config.desktop.system.mainMon.res;
+  mainMonPos = config.desktop.system.mainMon.pos;
+  # Main Monitor supported Refreshrates
   maxHz = config.desktop.system.mainMon.maxHz;
   midHz = config.desktop.system.mainMon.midHz;
   lowHz = config.desktop.system.mainMon.lowHz;
-  mainMonPos = config.desktop.system.mainMon.pos;
+  # Main Monitor Wallpapers
+  paperOne = config.desktop.system.mainMon.paperOne;
+  paperTwo = config.desktop.system.mainMon.paperTwo;
 in
 {
   home.packages = with pkgs;
   [(
     writeShellScriptBin "hyprTweaks"
     ''
-      function set_config()
+      function notify_error_and_exit()
+      {
+        value=$1
+        exitCode=$2
+        notifyGroup=$3
+        notifyMessage=$4
+        
+        if [[ "${value}" == "" ]]; then
+          ${dunstify} -u critical "$notifyGroup" "$notifyMessage"
+          exit $exitCode
+        fi
+      }
+      
+      function set_refreshrate_string()
+      {
+        preferredHz=$1
+        
+        case $preferredHz in
+          mid)
+            echo "${midHz}"
+            ;;
+          low)
+            echo "${lowHz}"
+            ;;
+          ?)
+            echo "${maxHz}"
+            ;;
+        esac
+      }
+      
+      function execute_command()
       {
         hyprCommand=$1
         notifyGroup=$2
         notifyMessage=$3
         
-        ${hyprctl} keyword "monitor ${mainMonName}, disable"
+        ${hyprctl} keyword "monitor ${mainMonDesc}, disable"
         
-        sleep 5s
+        sleep 2s
         
         ${hyprctl} --batch $hyprCommand
         
-        sleep 5s
+        sleep 2s
         
-        ${hyprctl} dispatch focusmonitor ${mainMonName}
+        ${hyprctl} dispatch focusmonitor ${mainMonDesc}
         
         ${xrandr} --output ${mainMonName} --primary
         
         ${dunstify} -u normal "$notifyGroup" "$notifyMessage"
       }
       
+      function reload_and_exit()
+      {
+        notifyGroup=$1
+        hyprCommand="reload;"
+        hyprCommand+="hyprpaper wallpaper ${mainMonDesc}, ${paperOne};"
+        
+        execute_command "$hyprCommand" "$notifyGroup" "Reloaded Hyprland"
+        
+        exit 0
+      }
+      
       adaptiveSync=false
       fullColorRange=false
+      refreshRate=""
       reload=false
-      preferredHz=""
       
       hyprCommand="keyword animations:enabled false;"
-      hyprCommand+=" keyword decoration:blur:enabled false;"
+      hyprCommand+="keyword decoration:blur:enabled false;"
       
       notifyGroup="HyprTweaks"
       notifyMessage="Tweaks on"
       
-      if [[ "${mainMonName}" == "" || "${mainMonRes}" == "" || "${maxHz}" == "" || "${midHz}" == "" || "${lowHz}" == "" || "${mainMonPos}" == "" ]]; then
-        ${dunstify} -u critical "$notifyGroup" "Missing main monitor config!"
-        exit 1
-      fi
+      notify_error_and_exit "${mainMonName}" "1" "$notifyGroup" "Missing Monitor Port!"
+      notify_error_and_exit "${mainMonDesc}" "2" "$notifyGroup" "Missing Monitor Description!"
+      notify_error_and_exit "${mainMonRes}" "3" "$notifyGroup" "Missing Monitor Resolution!"
+      notify_error_and_exit "${maxHz}" "4" "$notifyGroup" "Missing Monitor max Refreshrate!"
+      notify_error_and_exit "${midHz}" "5" "$notifyGroup" "Missing Monitor mid Refreshrate!"
+      notify_error_and_exit "${lowHz}" "6" "$notifyGroup" "Missing Monitor low Refreshrate!"
+      notify_error_and_exit "${mainMonPos}" "7" "$notifyGroup" "Missing Monitor Position!"
+      notify_error_and_exit "${paperOne}" "8" "$notifyGroup" "Missing Monitor default Wallpaper!"
+      notify_error_and_exit "${paperTow}" "9" "$notifyGroup" "Missing Monitor gaming Wallpaper!"
       
       while getopts "p:adhrst" opt; do
         case $opt in
@@ -67,12 +120,11 @@ in
             notifyMessage+=", 10bit"
             ;;
           p)
-            preferredHz=$OPTARG
+            refreshRate=$(set_refreshrate_string "$OPTARG")
+            notifyMessage+=", $refreshRate Hz"
             ;;
           r)
-            reload=true
-            preferredHz="mid"
-            notifyMessage="Reloaded Hyprland"
+            reload_and_exit "$notifyGroup"
             ;;
           s)
             hyprCommand+=" keyword cursor:no_hardware_cursors 1;"
@@ -83,27 +135,20 @@ in
             notifyMessage+=", Tear"
             ;;
           ?)
-            ${dunstify} -u critical "$notifyGroup" "Unknown parameters!"
-            exit 2
+            notify_error_and_exit "" "10" "$notifyGroup" "Unknown parameters!"
             ;;
         esac
       done
       
-      refreshRate="${maxHz}"
-      refreshRate=$([[ $preferredHz == "mid" ]] && echo "${midHz}" || echo $refreshRate)
-      refreshRate=$([[ $preferredHz == "low" ]] && echo "${lowHz}" || echo $refreshRate)
-      
-      notifyMessage+=", $refreshRate Hz"
-      
-      monitorConfig="monitor ${mainMonName}, ${mainMonRes}@$refreshRate, ${mainMonPos}, 1"
-      
-      $reload && ${hyprctl} reload && set_config "keyword $monitorConfig;" "$notifyGroup" "reloaded hyprland" && exit 0
+      monitorConfig="monitor ${mainMonDesc}, ${mainMonRes}@$refreshRate, ${mainMonPos}, 1"
       
       monitorConfig+=$($adaptiveSync && echo ", vrr, 2" || echo "")
       monitorConfig+=$($fullColorRange && echo ", bitdepth, 10" || echo "")
-      hyprCommand+=$($adaptiveSync || $bitdepth && echo " keyword $monitorConfig;" || echo "")
       
-      set_config "$hyprCommand" "$notifyGroup" "$notifyMessage"
+      hyprCommand+="keyword $monitorConfig;"
+      hyprCommand+="hyprpaper wallpaper ${mainMonDesc}, ${paperTow};"
+      
+      execute_command "$hyprCommand" "$notifyGroup" "$notifyMessage"
     ''
   )];
 }
