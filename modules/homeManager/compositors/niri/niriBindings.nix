eb60ffb64570dc43d5a036c8f910b60d13a93570
NeonGrim <michael.muellerdelossantos@outlook.com>
2025-11-21 02:53:05 +0100

    updated niri binds

--- modules/homeManager/compositors/niri/niriBindings.nix
+++ modules/homeManager/compositors/niri/niriBindings.nix
@@ -3,22 +3,101 @@ let
     mainMod = "Super";
     mainModShift = "Super+Shift";
     mainModCtrl = "Super+Ctrl";
+    mainModShiftCtrl = "Super+Shift+Ctrl";
     mainModAlt = "Super+Alt";
     mainModShiftAlt = "Super+Shift+Alt";
-    mainModCtrlAlt = "Super+Ctrl+Alt";
 in
 {
   desktop.niri.binds =
-  ''
-    binds {
+  let
+    appBinds =
+    ''
       ${mainModShift}+O { show-hotkey-overlay; }
-      
-      ${mainMod}+Return hotkey-overlay-title="Open a Terminal: Kitty" { spawn "${lib.getExe pkgs.kitty}"; }
-      ${mainMod}+E      hotkey-overlay-title="Open a File Explorer: Nemo" { spawn "${lib.getExe' pkgs.nemo-with-extensions "nemo"}"; }
-      
-      ${mainMod}+R hotkey-overlay-title="App Menu: Wofi" { spawn-sh "pkill wofi; sleep 0.1 && wofi -S drun -I --width 500 --height 600"; }
-      ${mainMod}+L hotkey-overlay-title="Power Menu: Wofi" { spawn-sh "pkill wofi; sleep 0.1 && wofiPowerMenu"; }
-      
+      ${mainMod}+Escape allow-inhibiting=false                     { toggle-keyboard-shortcuts-inhibit; }
+      ${mainMod}+Return hotkey-overlay-title="Terminal: Kitty"     { spawn "${lib.getExe pkgs.kitty}"; }
+      ${mainMod}+E      hotkey-overlay-title="File Explorer: Nemo" { spawn "${lib.getExe' pkgs.nemo-with-extensions "nemo"}"; }
+      ${mainMod}+R      hotkey-overlay-title="App Menu: Wofi"      { spawn-sh "pkill wofi; sleep 0.1 && wofi -S drun -I --width 500 --height 600"; }
+      ${mainMod}+L      hotkey-overlay-title="Power Menu: Wofi"    { spawn-sh "pkill wofi; sleep 0.1 && wofiPowerMenu"; }
+    '';
+    appFocusBinds =
+    ''
+      ${mainMod}+Tab repeat=false    { toggle-overview; }
+      ${mainMod}+q repeat=false      { close-window; }
+      ${mainModShift}+V              { switch-focus-between-floating-and-tiling; }
+      ${mainMod}+Home                { focus-column-first; }
+      ${mainMod}+End                 { focus-column-last; }
+      ${mainMod}+Right               { focus-column-right; }
+      ${mainMod}+Left                { focus-column-left; }
+      ${mainMod}+Up                  { focus-window-up; }
+      ${mainMod}+Down                { focus-window-down; }
+      ${mainMod}+WheelScrollRight    { focus-column-right; }
+      ${mainMod}+WheelScrollLeft     { focus-column-left; }
+      ${mainModCtrl}+WheelScrollUp   { focus-column-left; }
+      ${mainModCtrl}+WheelScrollDown { focus-column-right; }
+    '';
+    appPositionBinds =
+    ''
+      ${mainMod}+C                        { center-column; }
+      ${mainMod}+V                        { toggle-window-floating; }
+      ${mainModShift}+Home                { move-column-to-first; }
+      ${mainModShift}+End                 { move-column-to-last; }
+      ${mainModShift}+Right               { move-column-right; }
+      ${mainModShift}+Left                { move-column-left; }
+      ${mainModShift}+Up                  { move-window-up; }
+      ${mainModShift}+Down                { move-window-down; }
+      ${mainModShift}+WheelScrollLeft     { move-column-left; }
+      ${mainModShift}+WheelScrollRight    { move-column-right; }
+      ${mainModShiftCtrl}+WheelScrollUp   { move-column-left; }
+      ${mainModShiftCtrl}+WheelScrollDown { move-column-right; }
+    '';
+    appSizeBinds =
+    ''
+      ${mainMod}+D         { switch-preset-column-width; }
+      ${mainModShift}+D    { expand-column-to-available-width; }
+      ${mainModCtrl}+D     { maximize-column; }
+      ${mainMod}+A         { set-column-width "50%"; }
+      ${mainMod}+W         { switch-preset-window-height; }
+      ${mainMod}+S         { reset-window-height; }
+      ${mainMod}+F         { fullscreen-window; }
+      ${mainModShift}+F    { toggle-windowed-fullscreen; }
+      ${mainModCtrl}+Left  { consume-or-expel-window-left; }
+      ${mainModCtrl}+Right { consume-or-expel-window-right; }
+    '';
+    monitorBinds =
+    ''
+      ${mainModAlt}+Right      { focus-monitor-right; }
+      ${mainModAlt}+Left       { focus-monitor-left; }
+      ${mainModAlt}+Up         { focus-monitor-up; }
+      ${mainModAlt}+Down       { focus-monitor-down; }
+      ${mainModShiftAlt}+Right { move-column-to-monitor-right; }
+      ${mainModShiftAlt}+Left  { move-column-to-monitor-left; }
+      ${mainModShiftAlt}+Up    { move-column-to-monitor-up; }
+      ${mainModShiftAlt}+Down  { move-column-to-monitor-down; }
+      ${mainModShiftAlt}+P     { power-off-monitors; }
+    '';
+    workspaceBinds =
+    ''
+      ${mainMod}+Page_Up                              { focus-workspace-up; }
+      ${mainMod}+Page_Down                            { focus-workspace-down; }
+      ${mainMod}+WheelScrollUp cooldown-ms=150        { focus-workspace-up; }
+      ${mainMod}+WheelScrollDown cooldown-ms=150      { focus-workspace-down; }
+      ${mainModShift}+Page_Up                         { move-column-to-workspace-up; }
+      ${mainModShift}+Page_Down                       { move-column-to-workspace-down; }
+      ${mainModShift}+WheelScrollUp cooldown-ms=150   { move-column-to-workspace-up; }
+      ${mainModShift}+WheelScrollDown cooldown-ms=150 { move-column-to-workspace-down; }
+      ${mainModAlt}+Page_Up                           { move-workspace-up; }
+      ${mainModAlt}+Page_Down                         { move-workspace-down; }
+      ${mainModAlt}+WheelScrollUp cooldown-ms=150     { move-workspace-up; }
+      ${mainModAlt}+WheelScrollDown cooldown-ms=150   { move-workspace-down; }
+    '';
+    screenshotBinds =
+    ''
+      ${mainMod}+Print      { screenshot-screen; }
+      ${mainModShift}+Print { screenshot; }
+      ${mainModCtrl}+Print  { screenshot-window; }
+    '';
+    mediaBinds =
+    ''
       XF86AudioRaiseVolume allow-when-locked=true { spawn-sh "wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+"; }
       XF86AudioLowerVolume allow-when-locked=true { spawn-sh "wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"; }
       XF86AudioMute        allow-when-locked=true { spawn-sh "wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"; }
@@ -28,87 +107,16 @@ in
       XF86AudioNext        allow-when-locked=true { spawn-sh "playerctl next"; }
       XF86AudioRewind      allow-when-locked=true { spawn-sh "playerctl position 5-"; }
       XF86AudioForward     allow-when-locked=true { spawn-sh "playerctl position 5+"; }
-      
-      ${mainMod}+Tab repeat=false { toggle-overview; }
-      ${mainMod}+q   repeat=false { close-window; }
-      
-      ${mainMod}+Right { focus-column-right; }
-      ${mainMod}+Left  { focus-column-left; }
-      ${mainMod}+Up    { focus-window-up; }
-      ${mainMod}+Down  { focus-window-down; }
-      
-      ${mainModShift}+Right { move-column-right; }
-      ${mainModShift}+Left  { move-column-left; }
-      ${mainModShift}+Up    { move-window-up; }
-      ${mainModShift}+Down  { move-window-down; }
-      
-      ${mainMod}+Home      { focus-column-first; }
-      ${mainMod}+End       { focus-column-last; }
-      ${mainModShift}+Home { move-column-to-first; }
-      ${mainModShift}+End  { move-column-to-last; }
-      
-      ${mainModAlt}+Right { focus-monitor-right; }
-      ${mainModAlt}+Left  { focus-monitor-left; }
-      ${mainModAlt}+Up    { focus-monitor-up; }
-      ${mainModAlt}+Down  { focus-monitor-down; }
-      
-      ${mainModCtrl}+Right { move-column-to-monitor-right; }
-      ${mainModCtrl}+Left  { move-column-to-monitor-left; }
-      ${mainModCtrl}+Up    { move-column-to-monitor-up; }
-      ${mainModCtrl}+Down  { move-column-to-monitor-down; }
-      
-      ${mainMod}+Page_Up        { focus-workspace-up; }
-      ${mainMod}+Page_Down      { focus-workspace-down; }
-      ${mainModShift}+Page_Up   { move-column-to-workspace-up; }
-      ${mainModShift}+Page_Down { move-column-to-workspace-down; }
-      
-      ${mainModCtrl}+Page_Up   { move-workspace-up; }
-      ${mainModCtrl}+Page_Down { move-workspace-down; }
-      
-      ${mainMod}+WheelScrollUp        cooldown-ms=150 { focus-workspace-up; }
-      ${mainMod}+WheelScrollDown      cooldown-ms=150 { focus-workspace-down; }
-      ${mainModShift}+WheelScrollUp   cooldown-ms=150 { move-column-to-workspace-up; }
-      ${mainModShift}+WheelScrollDown cooldown-ms=150 { move-column-to-workspace-down; }
-      
-      ${mainMod}+WheelScrollRight      { focus-column-right; }
-      ${mainMod}+WheelScrollLeft       { focus-column-left; }
-      ${mainModShift}+WheelScrollRight { move-column-right; }
-      ${mainModShift}+WheelScrollLeft  { move-column-left; }
-      
-      ${mainModAlt}+WheelScrollUp       { focus-column-left; }
-      ${mainModAlt}+WheelScrollDown     { focus-column-right; }
-      ${mainModCtrlAlt}+WheelScrollUp   { move-column-left; }
-      ${mainModCtrlAlt}+WheelScrollDown { move-column-right; }
-      
-      ${mainModShiftAlt}+Left  { consume-or-expel-window-left; }
-      ${mainModShiftAlt}+Right { consume-or-expel-window-right; }
-      
-      ${mainModShiftAlt}+Up   { consume-window-into-column; }
-      ${mainModShiftAlt}+Down { expel-window-from-column; }
-      
-      ${mainMod}+D      { switch-preset-column-width; }
-      ${mainModShift}+D { maximize-column; }
-      ${mainModCtrl}+D  { expand-column-to-available-width; }
-      ${mainMod}+A      { set-column-width "50%"; }
-      ${mainMod}+W      { switch-preset-window-height; }
-      ${mainMod}+S      { reset-window-height; }
-      
-      ${mainMod}+F      { fullscreen-window; }
-      ${mainModShift}+F { toggle-windowed-fullscreen; }
-      
-      ${mainMod}+C      { center-column; }
-      ${mainModShift}+C { center-visible-columns; }
-      
-      ${mainMod}+V      { toggle-window-floating; }
-      ${mainModShift}+V { switch-focus-between-floating-and-tiling; }
-      
-      ${mainMod}+Print      { screenshot-screen; }
-      ${mainModShift}+Print { screenshot; }
-      ${mainModCtrl}+Print  { screenshot-window; }
-      
-      ${mainMod}+Escape allow-inhibiting=false { toggle-keyboard-shortcuts-inhibit; }
-      
-      ${mainModShiftAlt}+P { power-off-monitors; }
-    }
-  '';
+    '';
+  in
+  "binds {"+
+  appBinds+
+  appFocusBinds+
+  appPositionBinds+
+  appSizeBinds+
+  monitorBinds+
+  workspaceBinds+
+  screenshotBinds+
+  mediaBinds+
+  "}";
 }
