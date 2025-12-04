3b90aaa435bc463ab868696a830775850a34f657
NeonGrim <michael.muellerdelossantos@outlook.com>
2025-12-04 23:22:30 +0100

    removed unused changes

--- modules/nixos/compositors/greetd.nix
+++ modules/nixos/compositors/greetd.nix
@@ -1,43 +1,4 @@
-{pkgs, user, config, lib, ...}:
-let
-  # Home Manager Options
-  mainMonName = config.home-manager.users.${user}.desktop.mainMon.desc;
-  kbLayout = config.home-manager.users.${user}.desktop.system.kb.layout;
-  kbVariant = config.home-manager.users.${user}.desktop.system.kb.variant;
-  # Compositor Config
-  niriConf = pkgs.writeText "niriGtkGreetConfig" 
-  ''
-    spawn-sh-at-startup "swaybg -c 000000 & ${lib.getExe pkgs.regreet}; niri msg action quit --skip-confirmation"
-    hotkey-overlay {
-      skip-at-startup
-    }
-    gestures {
-      hot-corners {
-        off
-      }
-    }
-    input {
-      keyboard {
-        numlock
-        xkb {
-          layout "${kbLayout}"
-          variant "${kbVariant}"
-        }
-      }
-      mouse {
-        accel-speed 1
-        accel-profile "flat"
-      }
-    }
-    output "${mainMonName}" {
-      focus-at-startup
-      position x=0 y=0
-    }
-    window-rule {
-      open-on-output "${mainMonName}"
-    }
-  '';
-in
+{pkgs, user, lib, ...}:
 {
   environment.systemPackages = with pkgs;
   [
@@ -52,7 +13,7 @@ in
       {
         command = 
         (
-          "${pkgs.tuigreet}/bin/tuigreet --time --issue --remember --remember-session --user-menu --asterisks " +
+          "${pkgs.tuigreet}/bin/tuigreet --time --remember --remember-session --user-menu --asterisks " +
           "--theme border=magenta;text=cyan;prompt=green;time=red;action=blue;button=yellow;container=black;input=red"
         );
         user = "${user}";
