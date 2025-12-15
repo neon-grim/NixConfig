5f199dff520ae6e36f8125eaef10b23c881a0520
NeonGrim <michael.muellerdelossantos@outlook.com>
2025-12-15 22:45:27 +0100

    added winboat with podman config

--- modules/nixos/software/virtualisation.nix
+++ modules/nixos/software/virtualisation.nix
@@ -2,12 +2,18 @@
 {
   config = lib.mkIf (config.desktop.software.virtualization)
   {
+    environment.systemPackages = with pkgs;
+    [
+      winboat
+      podman-compose
+    ];
     programs.virt-manager =
     {
       enable = true;
     };
     virtualisation =
     {
+      podman.enable = true;
       libvirtd.enable = true;
       spiceUSBRedirection.enable = true;
     };
