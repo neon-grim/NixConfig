eda574a7a15fe09c15b2a8150eaef375c2d16e06
NeonGrim <michael.muellerdelossantos@outlook.com>
2026-03-10 01:22:16 +0100

    changed sources of certain packages

--- modules/nixos/software/ttyApps.nix
+++ modules/nixos/software/ttyApps.nix
@@ -1,24 +1,32 @@
-{pkgs-stable, ...}:
+{pkgs, pkgs-stable, ...}:
 {
-  environment.systemPackages = with pkgs-stable;
-  [
-    # Repo
-    git
-    lazygit
-    nix-prefetch-git
-    # Editor
-    neovim
-    # Utils
-    psmisc
-    usbutils
-    exiftool
-    # Packaging
-    unrar
-    unzip
-    # Media
-    wf-recorder
-    ffmpeg-full
-  ];
+  environment.systemPackages = 
+  (
+    with pkgs-stable;
+    [
+      # Repo
+      git
+      lazygit
+      nix-prefetch-git
+      # Editor
+      neovim
+      # Utils
+      psmisc
+      usbutils
+      exiftool
+      # Packaging
+      unrar
+      unzip
+    ]
+  ) ++ 
+  ( 
+    with pkgs-stable;
+    [
+      # Media
+      wf-recorder
+      ffmpeg-full
+    ]
+  );
   programs.fish =
   {
     enable = true;
